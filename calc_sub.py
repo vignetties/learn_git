518c8edcfd16f0b8c42a18e17009e93d73ef0ec1
vignetties <vignetties@gmail.com>
2021-04-20 23:18:44 +0530

    Create calc_sub.py

---
+++ calc_sub.py
@@ -0,0 +1,10 @@
+# This program sub two numbers
+
+num1 = 6.5
+num2 = 1.3
+
+# Add two numbers
+sum = num1 - num2
+
+# Display the sub
+print('The sub of {0} and {1} is {2}'.format(num1, num2, sum))
